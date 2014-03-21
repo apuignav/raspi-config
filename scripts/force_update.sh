7d27dcaba33b92c79d386eab5c0d8d6cc6babfbe
Albert Puig <albert.puig@cern.ch>
2014-03-21 20:37:00 +0100

    Updated force update script

--- scripts/force_update.sh
+++ scripts/force_update.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-sudo -s
 cd /scripts/upd_sys
-rm *.sh
-wget http://svn.stmlabs.com/svn/raspbmc/release/update-system/getfile.sh
-wget http://svn.stmlabs.com/svn/raspbmc/release/update-system/cdn_env_prep.sh
-reboot
+sudo rm *.sh
+sudo wget http://svn.stmlabs.com/svn/raspbmc/release/update-system/getfile.sh
+sudo wget http://svn.stmlabs.com/svn/raspbmc/release/update-system/cdn_env_prep.sh
+sudo reboot
