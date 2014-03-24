205aa7931265a8660f82992b67c18663f2d41a06
Albert Puig <albert.puig@cern.ch>
2014-03-24 21:28:51 +0100

    Rewrite of backup.py to allow for multiple services to backup

--- scripts/backup.py
+++ scripts/backup.py
@@ -11,18 +11,31 @@ import shutil
 import argparse
 
 def do_backup(args):
-    """Backup /home/pi/.xbmc dir to backup.tar.gz.
+    """Backup XBMC and deluge."""
+    def perform_backup(origin_folder, output_file, service=None):
+        """Backup folder.
 
-    The tar file will go to the folder specified in args.folder.
+        @arg  origin_folder: folder to backup
+        @type origin_folder: str
+        @arg  output_file: file to backup to
+        @type output_file: str
+        @arg  service: do I need to stop something?
+        @type service: str
 
-    """
-    output_file = os.path.join(args.folder, 'backup.tar.gz')
-    if os.path.exists(output_file):
-        os.remove(output_file)
-    os.system("sudo stop xbmc")
-    os.system("cd /home/pi && tar -cvzf %s .xbmc" % output_file)
-    os.system("sudo start xbmc")
-    print "Backed up to %s" % output_file
+        """
+        if os.path.exists(output_file):
+            os.remove(output_file)
+        base_folder, folder_to_backup = os.path.split(output_file)
+        if service:
+            os.system("sudo stop %s" % service)
+        os.system("cd %s && tar -cvzf %s %s" % (base_folder, output_file, folder_to_backup))
+        if service:
+            os.system("sudo start %s" % service)
+        print "Backed up to %s" % output_file
+
+    output_folder = os.path.abspath(args.folder)
+    perform_backup('/home/pi/.xbmc', os.path.join(output_folder, 'backup.xbmc.tar.gz'), 'xbmc')
+    perform_backup('/home/pi/.config/deluge', os.path.join(output_folder, 'backup.deluge.tar.gz'), 'deluge')
 
 def restore_backup(args):
     """Restore backup to  /home/pi/.xbmc.
@@ -30,14 +43,31 @@ def restore_backup(args):
     The tar file to restore is given by args.backup_folder.
 
     """
-    if not os.path.exists(args.backup_file):
-        print "Cannot find backup file %s" % args.backup_file
-        return
-    shutil.copy(args.backup_file, '/home/pi/')
-    os.system("sudo stop xbmc")
-    os.system("cd /home/pi && tar -xzf backup.tar.gz && rm backup.tar.gz")
-    os.system("sudo start xbmc")
-    print "Restored backup from %s" % args.backup_file
+    def perform_restore(backup_file, output_folder, service=None):
+        """Restore folder.
+
+        @arg  backup_file: file to restore
+        @type backup_file: str
+        @arg  output_folder: file to restore to
+        @type output_folder: str
+        @arg  service: do I need to stop something?
+        @type service: str
+
+        """
+        if not os.path.exists(backup_file):
+            print "Cannot find backup file %s" % backup_file
+            return
+        base_folder, _ = os.path.split(output_folder)
+        if service:
+            os.system("sudo stop %s" % service)
+        os.system("cd %s && tar -xzf %s" % (base_folder, backup_file))
+        if service:
+            os.system("sudo start %s" % service)
+        print "Restored backup from %s" % args.backup_file
+
+    backup_folder = os.path.abspath(args.folder)
+    perform_restore(os.path.join(backup_folder, 'backup.xbmc.tar.gz'), '/home/pi/.xbmc', 'xbmc')
+    perform_restore(os.path.join(backup_folder, 'backup.deluge.tar.gz'), '/home/pi/.config/deluge', 'deluge')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -48,7 +78,7 @@ if __name__ == '__main__':
     backup_parser.set_defaults(func=do_backup)
     # Parser for the restore command
     restore_parser = subparsers.add_parser("restore")
-    restore_parser.add_argument('backup_file', action='store', type=str, help="Backup file to restore from")
+    restore_parser.add_argument('folder', action='store', type=str, help="Backup folder to restore from")
     restore_parser.set_defaults(func=restore_backup)
     # Parse!
     args = parser.parse_args()
