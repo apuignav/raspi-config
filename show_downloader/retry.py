883b5ced53367a77faf10a84e8401fa7068e5cf5
Albert Puig <albert.puig@cern.ch>
2016-05-31 10:12:46 +0200

    Add retry option to show downloading

---
+++ show_downloader/retry.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# =============================================================================
+# @file   retry.py
+# @author Albert Puig (albert.puig@cern.ch)
+# @date   31.05.2016
+# =============================================================================
+"""Retry decorator from the python wiki."""
+
+import time
+from functools import wraps
+
+
+def retry(ExceptionToCheck, tries=4, delay=3, backoff=2):
+    """Retry calling the decorated function using an exponential backoff.
+
+    http://www.saltycrane.com/blog/2009/11/trying-out-retry-decorator-python/
+    original from: http://wiki.python.org/moin/PythonDecoratorLibrary#Retry
+
+    Arguments:
+        ExceptionToCheck (Exception, tuple): the exception to check. may be a tuple of
+            exceptions to check
+
+        tries (int): number of times to try (not retry) before giving up
+        delay (int): initial delay between retries in seconds
+        backoff (int) backoff multiplier e.g. value of 2 will double the delay
+            each retry
+
+    """
+    def deco_retry(f):
+
+        @wraps(f)
+        def f_retry(*args, **kwargs):
+            mtries, mdelay = tries, delay
+            while mtries > 1:
+                try:
+                    return f(*args, **kwargs)
+                except ExceptionToCheck, error:
+                    msg = "%s, Retrying in %d seconds..." % (str(error), mdelay)
+                    logging.warning(msg)
+                    time.sleep(mdelay)
+                    mtries -= 1
+                    mdelay *= backoff
+            return f(*args, **kwargs)
+
+        return f_retry  # true decorator
+
+    return deco_retry
+
+# EOF
